6243c3422a1250fe52e271772aee77e34d3a426e
Tung <tungptkh@gmail.com>
2017-08-11 13:54:13 +0700

    update block other ip

--- src/containers/upload-image/index.js
+++ src/containers/upload-image/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import ImageForm from "./ImageForm";
+import { connectAutoBindAction } from "utils/redux";
 
 const ImageContainer = styled.div`
    min-height: 100vh;
@@ -20,9 +21,34 @@ const Container = styled.div`
    }
 `;
 
+@connectAutoBindAction(state => ({
+  clientIp: state.store.clientIp,
+  stores: state.store.list.data,
+  isLogined: state.user.me.isLogined
+}))
 export default class UploadImageContainer extends Component {
   static propTypes = {};
+  checkStoreHaveIp() {
+    let check = false;
+    if (this.props.stores.length > 0) {
+      this.props.stores.forEach(store => {
+        if (store.ip.indexOf(this.props.clientIp) > -1) {
+          check = true;
+        }
+      });
+    }
+    return check;
+  }
+
+  checkIpVerify() {
+    if (this.props.isLogined) {
+      return true;
+    }
+    return this.checkStoreHaveIp();
+  }
+
   render() {
+    if (!this.checkIpVerify()) return null;
     return (
       <ImageContainer>
         <Container>
