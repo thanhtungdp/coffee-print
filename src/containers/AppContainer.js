3f52b6ef4b9f13dbe297467f7af85243fefaf259
Tung <tungptkh@gmail.com>
2017-08-15 21:07:22 +0700

    add update

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { connectAutoBindAction } from "utils/redux";
 import { getClientIp, getStores } from "redux/actions/storeAction";
 
