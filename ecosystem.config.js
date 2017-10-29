348e0607dd8303a151f7ec41613191d056389ea0
Phan Thanh Tung <tungptkh@gmail.com>
2017-10-29 16:32:59 +0700

    update deloy pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**
+	 * Application configuration section
+	 * http://pm2.keymetrics.io/docs/usage/application-declaration/
+	 */
+  apps: [
+    // First application
+    {
+      name: 'coffee_print',
+      script: './run.sh',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+	 * Deployment section
+	 * http://pm2.keymetrics.io/docs/usage/deployment/
+	 */
+  deploy: {
+    production: {
+      user: 'release',
+      host: '188.166.255.80',
+      ref: 'origin/release/production-inhinh',
+      repo: 'ssh://git@github.com:thanhtungdp/coffee-print.git',
+      path: '/data/pm2/coffee_print',
+      'post-deploy': 'yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
